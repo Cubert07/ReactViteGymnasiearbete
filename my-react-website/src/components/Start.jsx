b803ee63d59286e0be1137af4465fa529766311f
Cuber07 <albertahlqvist@gmail.com>
2025-10-17 16:50:21 +0200

    skapade bas struktur för React websidan

---
+++ my-react-website/src/components/Start.jsx
@@ -0,0 +1,10 @@
+export default function Start() {
+    return (
+        <div>
+        <h1 className="text-3xl font-bold underline">Getting Started with React</h1>
+        <p className="mt-4 text-lg">
+            This page will help you get started with building React applications.
+        </p>
+        </div>
+    );
+}
---
+++ my-react-website/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import Start from '../components/Start';
+export default function Home() {
+  return (
+    <div className="p-6">
+      <Start />
+    </div>
+  );
+}
