26b7c0ec1e8e02223890ff9d07172c1b07047324
Victor-Johansson <victor.johansson@elev.ga.ntig.se>
2025-10-17 16:58:49 +0200

    Nånting hände

--- my-react-website/src/App.jsx
+++ my-react-website/src/App.jsx
@@ -1,21 +1,9 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-
 function App() {
   return (
     <div>
-      <nav className="p-4 bg-gray-100">
-        <Link to="/" className="mr-4">Home</Link>
-        <Link to="/about" className="mr-4">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<div className="p-6">Page not found</div>} />
-      </Routes>
+      <h1>Hello, this is my React website!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
